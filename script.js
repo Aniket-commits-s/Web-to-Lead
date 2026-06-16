f4eac66e5f5de9d51144ef92627afe078e1a5359
Aniket Mishra <aniketmis8218@gmail.com>
2026-06-16 14:50:28 +0530

    Added the captcha with style

--- script.js
+++ script.js
@@ -4,4 +4,17 @@ function beforeHandler() {
   let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
   outputdate.value = formatteddate;
   console.log("Input value is mentioned in the side", inputdate.value);
+
+  function timestamp() {
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+      var elems = JSON.parse(
+        document.getElementsByName("captcha_settings")[0].value,
+      );
+      elems["ts"] = JSON.stringify(new Date().getTime());
+      document.getElementsByName("captcha_settings")[0].value =
+        JSON.stringify(elems);
+    }
+  }
+  setInterval(timestamp, 500);
 }
