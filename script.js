ac3bd999664dc3977583220d916d2b172eafa12d
Aniket Mishra <aniketmis8218@gmail.com>
2026-06-17 11:18:41 +0530

    Change to old code of js

--- script.js
+++ script.js
@@ -1,39 +1,29 @@
 let captchachecked = false;
-
-function captchasuccess() {
-  captchachecked = true;
-}
-
 function beforeHandler(event) {
-  if (!captchachecked) {
+  if (captchachecked) {
+    let outputdate = document.querySelector(".outputdate");
+    let inputdate = document.querySelector(".inputdate");
+    let formatteddate = new Date(inputdate.value).toLocaleDateString("en-IN");
+    outputdate.value = formatteddate;
+    console.log("Input value is mentioned in the side", inputdate.value);
+  } else {
     alert("Please check the captcha to submit the lead");
     event.preventDefault();
-    return false;
   }
+  function timestamp() {
+    var response = document.getElementById("g-recaptcha-response");
+    if (response == null || response.value.trim() == "") {
+      var elems = JSON.parse(
+        document.getElementsByName("captcha_settings")[0].value,
+      );
+      elems["ts"] = JSON.stringify(new Date().getTime());
+      document.getElementsByName("captcha_settings")[0].value =
+        JSON.stringify(elems);
+    }
+  }
+  setInterval(timestamp, 500);
 
-  let outputdate = document.querySelector(".outputdate");
-  let inputdate = document.querySelector(".inputdate");
-
-  outputdate.value = inputdate.value;
-
-  console.log(outputdate.value);
-
-  return true;
-}
-
-function timestamp() {
-  var response = document.getElementById("g-recaptcha-response");
-
-  if (response == null || response.value.trim() == "") {
-    var elems = JSON.parse(
-      document.getElementsByName("captcha_settings")[0].value,
-    );
-
-    elems["ts"] = JSON.stringify(new Date().getTime());
-
-    document.getElementsByName("captcha_settings")[0].value =
-      JSON.stringify(elems);
+  function captchasuccess() {
+    captchachecked = true;
   }
 }
-
-setInterval(timestamp, 500);
